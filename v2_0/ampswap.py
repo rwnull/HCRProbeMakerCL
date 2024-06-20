e070b54b233a6ec87458c45354d4107bf7fd63f3
rwnull <65666230+rwnull@users.noreply.github.com>
2024-06-20 11:29:19 -0500

    small bug fix in ampswap and mainscript

--- v2_0/ampswap.py
+++ v2_0/ampswap.py
@@ -9,8 +9,8 @@ import datetime
 parser = argparse.ArgumentParser()
  
 # Adding optional arguments
-parser.add_argument("-oldAmp", "--Old_amplifier", help = "*REQUIRED INPUT*   Indicate the old amplifier initiator sequence you want to replace in these probes. Supports B1-B5,B7,B9,B10,B11,B13,B14,B15,B17,S10,S23,S25,S34,S35, and S41. Example: -oldAmp B1 ")
-parser.add_argument("-newAmp", "--New_amplifier", help = "*REQUIRED INPUT*   Indicate the replacement amplifier. Supports B1-B5,B7,B9,B10,B11,B13,B14,B15,B17,S10,S23,S25,S34,S35, and S41. Example: -newAmp B5 ")
+parser.add_argument("-old","-oldAmp", "--Old_amplifier", help = "*REQUIRED INPUT*   Indicate the old amplifier initiator sequence you want to replace in these probes. Supports B1-B5,B7,B9,B10,B11,B13,B14,B15,B17,S10,S23,S25,S34,S35, and S41. Example: -oldAmp B1 ")
+parser.add_argument("-new","-newAmp", "--New_amplifier", help = "*REQUIRED INPUT*   Indicate the replacement amplifier. Supports B1-B5,B7,B9,B10,B11,B13,B14,B15,B17,S10,S23,S25,S34,S35, and S41. Example: -newAmp B5 ")
 parser.add_argument("-in", "--in_file", help = "*REQUIRED INPUT*   Provide the path to the input file is. Example: '/home/PathtoInput/Opool/OldInitiatorProbes.xlsx' Must be in IDT opool submission format.")
 
 # Initialization of optional variables
@@ -51,12 +51,12 @@ MEDIA_ROOT,BLAST_ROOT,MEDIA_FASTA,MEDIA_OPOOL,MEDIA_OLIGO,MEDIA_TXT = roots[0],r
 
 
 def newname(names,old,new):
-    sys.tracebacklimit=0
+    #sys.tracebacklimit=0
     newname = str(names).replace(str(old),str(new))
-    return newname
+    return(newname)
 
 def swap(seq,old,new,dire):
-    sys.tracebacklimit=0
+    #sys.tracebacklimit=0
     seq = pd.ExcelFile(seq,"openpyxl")
     seqseq = list(seq.parse()["Sequence"])
     seqnm = list(seq.parse()["Pool name"])
@@ -65,44 +65,47 @@ def swap(seq,old,new,dire):
     seqs = len(seqseq)
     sc = "25nm"
     pu = "STD"
-    p=0
+    
+    #print(new)
+    for newinit in new:
+        p=0
+        #print(newinit)
+        while p < seqs:
+            for sequence in seqseq:
+                if str(amps[old][0]) in str(sequence):
+                    new0 = str(sequence).replace(str(amps[old][0]),str(amps[newinit][0]))
+                    #print(new0)#,seqs)
+                    name = newname(seqnm[p],old,newinit)
+                    oname1 = str(name+'_'+str(p+1)+'A')
+                    newseqs[p] = [str(name),str(new0)]
+                    newoligoseqs[p]= [str(oname1),str(new0),sc,pu]
+                    p+=1
+                elif str(amps[old][1]) in str(sequence):
+                    new1 = str(sequence).replace(str(amps[old][1]),str(amps[newinit][1]))
+                    #print(new1)
+                    name = newname(seqnm[p],old,newinit)
+                    oname2 = str(name+'_'+str(p)+'B')
+                    newseqs[p] = [str(name),str(new1)]
+                    newoligoseqs[p]=[str(oname2),str(new1),sc,pu]
+                    p+=1     
+                else:
+                    name =seqnm[p]
+                    newseqs[p] = [str(name),str(sequence)]
+                    newoligoseqs[p]= [str(name),str(sequence),sc,pu]
+                    p+=1  
+        #
+        x = pd.DataFrame(data=newseqs, columns=["Pool name","Sequence"])
+        x = x.set_index("Pool name")  
+        x.to_excel(os.path.join(dire,(name+"_OPool_initiatorswap.xlsx")))
 
-    while p < seqs:
-        for sequence in seqseq:
-            if str(amps[old][0]) in str(sequence):
-                new0 = str(sequence).replace(str(amps[old][0]),str(amps[new][0]))
-                #print(new0,seqs)
-                name = newname(seqnm[p],old,new)
-                oname1 = str(name+'_'+str(p+1)+'A')
-                newseqs[p] = [str(name),str(new0)]
-                newoligoseqs[p]= [str(oname1),str(new0),sc,pu]
-                p+=1
-            elif str(amps[old][1]) in str(sequence):
-                new1 = str(sequence).replace(str(amps[old][1]),str(amps[new][1]))
-                #print(new1)
-                name = newname(seqnm[p],old,new)
-                oname2 = str(name+'_'+str(p)+'B')
-                newseqs[p] = [str(name),str(new1)]
-                newoligoseqs[p]=[str(oname2),str(new1),sc,pu]
-                p+=1     
-            else:
-                name =seqnm[p]
-                newseqs[p] = [str(name),str(sequence)]
-                newoligoseqs[p]= [str(name),str(sequence),sc,pu]
-                p+=1  
-    #
-    x = pd.DataFrame(data=newseqs, columns=["Pool name","Sequence"])
-    x = x.set_index("Pool name")  
-    x.to_excel(os.path.join(dire,(name+"_OPool_initiatorswap.xlsx")))
+        y = pd.DataFrame(data=newoligoseqs, columns=["Name","Sequence","Scale","Purification"])
+        y = y.set_index("Name")
+        y.to_excel(os.path.join(dire,(name+"_OligoOrder_initiatorswap.xlsx")))
 
-    y = pd.DataFrame(data=newoligoseqs, columns=["Name","Sequence","Scale","Purification"])
-    y = y.set_index("Name")
-    y.to_excel(os.path.join(dire,(name+"_OligoOrder_initiatorswap.xlsx")))
-
-    with open(os.path.join(dire,(name+"ReadMe_initiatorswap.txt")),"w") as z:
-        z.write(vers()+"\r\n\r\n")
-        z.write("This file serves as a reminder that oligos for use with "+str(new)+" were created from: "+str(old)+", on "+str(datetime.date.today()))
-        z.close    
+        with open(os.path.join(dire,(name+"ReadMe_initiatorswap.txt")),"w") as z:
+            z.write(vers()+"\r\n\r\n")
+            z.write("This file serves as a reminder that oligos for use with "+str(newinit)+" were created from: "+str(old)+", on "+str(datetime.date.today()))
+            z.close    
 
     return(x,y)
 
@@ -110,11 +113,16 @@ args = parser.parse_args()
 if args.Old_amplifier:
     old = str(args.Old_amplifier).upper()
 if args.New_amplifier:
-    new = str(args.New_amplifier).upper()
+    if len(list(args.New_amplifier)) == 1:
+        new = list(str(args.New_amplifier).upper())
+        #print(new)
+    else:
+        new = list(str(args.New_amplifier).upper().split(','))
+        #print(new)
 if args.in_file:
     seq = os.path.abspath(str(args.in_file))
     dire,file = os.path.split(seq)
-    print(dire)
+    #print(dire)
     if os.path.exists(os.path.join(dire,"AmplifierSwap")):
         dire = os.path.join(dire,"AmplifierSwap")
     else:
